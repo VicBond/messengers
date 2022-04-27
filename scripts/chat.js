30495d4cdedaf8e55f0083c8d9b63c947ca13705
Victor Bondarenko <nirasimha@gmail.com>
2022-04-27 15:56:45 +0000

    return response

--- scripts/chat.js
+++ scripts/chat.js
@@ -7,14 +7,18 @@ class Chatroom {
   async addChat(message){
       const now = new Date();
       const chat = {
-        created: firebase.firestore.Timestamp.fromDate(now),
+        created_at: firebase.firestore.Timestamp.fromDate(now),
         username: this.username,
         message,
         room: this.room,
       };
-      
+      const response = await this.chats.add(chat);
+      return response;
   }
 }
 
 const chatroom = new Chatroom('gaming', 'shaun');
-console.log(chatroom);
+// console.log(chatroom);
+chatroom.addChat('hello everyone')
+  .then(() => console.log('chat added'))
+  .catch(err => console.log(err));
