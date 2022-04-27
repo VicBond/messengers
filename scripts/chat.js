7cbb7897fd45e6511431bb90171b66187ee5a732
Victor Bondarenko <nirasimha@gmail.com>
2022-04-27 14:54:37 +0000

    add asynch method addChat

--- scripts/chat.js
+++ scripts/chat.js
@@ -4,7 +4,16 @@ class Chatroom {
     this.username = username;
     this.chats = db.collection('chats');
   }
-
+  async addChat(message){
+      const now = new Date();
+      const chat = {
+        created: firebase.firestore.Timestamp.fromDate(now),
+        username: this.username,
+        message,
+        room: this.room,
+      };
+      
+  }
 }
 
 const chatroom = new Chatroom('gaming', 'shaun');
