48abbaa66e96e461acdb3453787d053861c59831
Victor Bondarenko <nirasimha@gmail.com>
2022-05-02 10:02:39 +0000

    add ChatUI class instance

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,6 @@
 const chatList = document.querySelector('.chat-list');
 
+const chatUI = new ChatUI(chatList);
 const chatroom = new Chatroom('gaming', 'shaun');
 // console.log(chatroom);
 // chatroom.addChat('hello everyone')
