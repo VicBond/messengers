8889604175ee364b8d1a0348edff420398eb3f57
Victor Bondarenko <nirasimha@gmail.com>
2022-05-03 14:40:31 +0000

    add settimeout to name update mssg

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 const chatList = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
 const newNameForm = document.querySelector('.new-name');
+const updateMssg = document.querySelector('.update-mssg');
 
 newChatForm.addEventListener('submit', e => {
   e.preventDefault();
@@ -10,13 +11,15 @@ newChatForm.addEventListener('submit', e => {
     .catch(err => console.log(err));
 });
 
-newNameForm.addEventListener('submit', () => {
+newNameForm.addEventListener('submit', e => {
   e.preventDefault();
   const newName = newNameForm.name.value.trim();
   chatroom.updateName(newName);
 
   newNameForm.reset();
 
+  updateMssg.innerText = `Your name was updated to ${newName}`;
+  setTimeout(() => updateMssg.innerText = '', 3000);
 });
 
 const chatUI = new ChatUI(chatList);
